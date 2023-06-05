ecb315644f8ef3a6c9f337f78cf7cdd9d3e1fd06
fanrui07 <fanrui07@maoyan.com>
2023-06-05 15:02:59 +0800

    路由参数回传

--- src/navigator/home.tsx
+++ src/navigator/home.tsx
@@ -1,31 +1,67 @@
 import * as React from "react";
-import {Button, Text, View} from "react-native";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {PassingParameters} from "./passing_parameters";
 
 interface Props {
-    navigation: any
+    navigation: any,
+    route: any,
 }
 
 interface State {
-
+    title: string
 }
 
 export class HomeScreen extends React.Component<Props, State> {
-    render() {
-        React.useEffect(() => {
 
-        })
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            title: 'Home Screen',
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        const currentParams = this.props.route.params
+        const preParams = prevProps.route.params
+        if (currentParams !== preParams) {
+            this.setState({title: 'Home Screen: ' + currentParams?.post})
+        }
+    }
+
+    render() {
+
         return (
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Home Screen</Text>
-                <Button title='Go to Details'
+                <Text>{this.state.title}</Text>
+                <TouchableOpacity style={styles.button}
                         onPress={() => this.props.navigation.navigate('Details', {
                             itemId: 86,
                             otherParam: 'anything you want here',
-                        })}/>
-                <Button title='Go To PassingParameters'
-                        onPress={()=> this.props.navigation.navigate('PassingParameters')}/>
+                        })}>
+                    <Text style={styles.button_text}>Go to Details</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                                  onPress={()=> this.props.navigation.navigate('PassingParameters')}>
+                    <Text style={styles.button_text}>Go To PassingParameters</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    button: {
+        padding: 10,
+        backgroundColor: "#4497f5",
+        borderRadius: 5,
+        marginVertical: 5,
+    },
+    button_text: {
+        color: "#FFFFFF"
+    },
+})
